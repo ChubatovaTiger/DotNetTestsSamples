8411060e10455c41b86d7a16269722ccaf23fe27
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-12-20 18:12:07 +0100

    Update NUnitTest.cs

--- TestProject2/NUnitTest.cs
+++ TestProject2/NUnitTest.cs
@@ -4,51 +4,15 @@ namespace TestProject2
 {
     public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
-        [Category("Short")]
-        public void Test1NunitSimplever1()
+
+        public void VerySimpleTest()
         {
             Assert.Fail();
         }
-        
-        
-        [Test]
-         [Category("Short")]
-        [TestCase("chrome", "72.0", "Windows 10")]
-        [TestCase("internet explorer", "11.0", "Windows 10")]
-        public void Test2(string str1, string str2, string str3)
-        {
-            Assert.Pass();
-        }
-        
-
-        
-        [Test, Sequential]
-         [Category("Long")]
-        public void NunitSequentialParametersTest(
-         [Values(1,2,3)] int x,
-        [Values("A","B")] string s)
-        {
-         Assert.Pass();
-        }
-
-        
-[Test]
-[Category("Long")]
-public void NUnitRangeTest(
-    [Values(1, 2, 3)] int x,
-    [Range(0.2, 0.6, 0.2)] double d)
-{
-    Assert.Pass();
-}
-     
-        
-        
+              
+              
         
     }
 }
