63054dd412c8dcb3b19e54a0932b58cb7f4c68d3
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-12-20 20:41:39 +0100

    Update NUnitTest.cs

--- TestProject2/NUnitTest.cs
+++ TestProject2/NUnitTest.cs
@@ -1,10 +1,12 @@
+using NUnit.Framework;
+
 namespace TestProject2
 {
     public class NUnitTest1wthDisplN
     {
 
 
-[TestCase(5,TestName="dsplNameNUNitver123")]
+[TestCase(5,TestName="dsplNameNUNitver1")]
 public void TestCasewithDsplMNameNunitver1() {
     Assert.AreEqual(5, 4);
 }
