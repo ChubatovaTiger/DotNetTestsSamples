aa2035d34f7e334652a0e86a49d4946a5ef636e1
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-03 16:56:49 +0200

    Update NUnitTest.cs

--- TestProject2/NUnitTest.cs
+++ TestProject2/NUnitTest.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+
 using System;
 using System.Threading;
 
@@ -6,54 +6,9 @@ namespace TestProject2
 {
     public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
- //[Ignore("Ignore a test")]
-        [Test]
-        public void Test1NunitSimplever1()
-        {
+      
             Thread.Sleep(2000);
-            Assert.Fail();
-        }
-        
-         //[Ignore("Ignore a test")]
-        [Test]
-         [Category("Short")]
-        [TestCase("chrome", "72.0", "Windows 10")]
-        [TestCase("internet explorer", "11.0", "Windows 10")]
-        public void Test2(string str1, string str2, string str3)
-        {
-            Thread.Sleep(2000);
-            Assert.Pass();
-        }
-        
 
-         //[Ignore("Ignore a test")]
-        [Test, Sequential]
-         [Category("Long")]
-        public void NunitSequentialParametersTest(
-         [Values(1,2,3)] int x,
-        [Values("A","B")] string s)
-        {
-            Thread.Sleep(2000);
-         Assert.Pass();
         }
-
-     //[Ignore("Ignore a test")]    
-[Test]
-[Category("Long")]
-public void NUnitRangeTest(
-    [Values(1, 2, 3)] int x,
-    [Range(0.2, 0.6, 0.2)] double d)
-{
-    Thread.Sleep(2000);
-    Assert.Pass();
-}
-     
-        
-        
-        
-    }
+       
 }
