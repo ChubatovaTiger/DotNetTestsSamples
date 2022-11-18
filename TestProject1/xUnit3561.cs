7234e1a8d14fe74a5d38c7f438e2c65256a63087
AChubatova <n.chubatova@gmail.com>
2022-11-18 01:00:37 -0800

    100000

---
+++ TestProject1/xUnit3561.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.Contracts;
+using Xunit;
+using System.Threading;
+
+
+namespace TestProject1
+{
+    public class UnitTest3561
+    {
+       [Fact]
+        public void Test1()
+        {
+        }
+        [Fact]
+        public void Test2()
+        {
+        }
+        [Fact]
+        public void Test3()
+        {
+        }
+        [Fact]
+        public void Test4()
+        {
+        }
+        [Fact]
+        public void Test5()
+        {
+        }
+        [Fact]
+        public void Test6()
+        {
+        }
+        [Fact]
+        public void Test7()
+        {
+        }
+        [Fact]
+        public void Test8()
+        {
+        }
+        [Fact]
+        public void Test9()
+        {
+        }
+        [Fact]
+        public void Test10()
+        {
+        }
+    }
+}
