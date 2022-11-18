1ecccdfbc67ebcbd208266b6836aefcfba78fa58
AChubatova <n.chubatova@gmail.com>
2022-11-18 02:54:06 -0800

    50ms

--- TestProject1/xUnit9963.cs
+++ TestProject1/xUnit9963.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics.Contracts;
 using Xunit;
+using System;
 using System.Threading;
 
 
@@ -13,42 +14,61 @@ namespace TestProject1
        [Fact]
         public void Test1()
         {
+Thread.Sleep(50);
         }
         [Fact]
         public void Test2()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test3()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test4()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test5()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test6()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test7()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test8()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test9()
         {
+Thread.Sleep(50);
+     
         }
         [Fact]
         public void Test10()
         {
+Thread.Sleep(50);
+     
         }
     }
 }
