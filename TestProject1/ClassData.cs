c28c70f3488487329ba4f6189a0a3ba01f42af7f
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-02-01 21:10:04 +0100

    Update ClassData.cs

--- TestProject1/ClassData.cs
+++ TestProject1/ClassData.cs
@@ -21,7 +21,7 @@ namespace TestProject1
         {
             public IEnumerator<object[]> GetEnumerator()
             {
-                for (int i = 0; i < 500; i++)
+                for (int i = 0; i < 50000; i++)
                 {
                     yield return new object[] { new Employee { FirstName = i.ToString(), LastName = i.ToString() } };
                 }
