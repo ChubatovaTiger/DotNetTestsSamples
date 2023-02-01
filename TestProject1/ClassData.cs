8e97d51396cf61404379d94af6b7767800ba9156
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-02-01 13:48:51 +0100

    Update ClassData.cs

--- TestProject1/ClassData.cs
+++ TestProject1/ClassData.cs
@@ -8,35 +8,18 @@ using Xunit;
 
 namespace TestProject1
 {
-public class TestDataGenerator : IEnumerable<object[]>
-{
-    private readonly List<object[]> _data = new List<object[]>
-    {
-        new object[] {5, 1, 3, 9000},
-        new object[] {7, 1, 5, 3}
-    };
-    List.add({5, 1, 3, 9000});
+    public static IEnumerable<object[]> enumValues()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return new object[] { i };
+            }
+        }
 
-    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-}
-
-public class ParameterizedTests
-{
-    public bool IsOddNumber(int number)
-    {
-        return number % 2 != 0;
-    }
-
-    [Theory]
-    [ClassData(typeof(TestDataGenerator))]
-    public void AllNumbers_AreOdd_WithClassData(int a, int b, int c, int d)
-    {
-        Assert.True(IsOddNumber(a));
-        Assert.True(IsOddNumber(b));
-        Assert.True(IsOddNumber(c));
-        Assert.True(IsOddNumber(d));
-    }
-}
+ [Theory]
+        [MemberData("enumValues")]
+        public void TestNumberGeneric(Number number)
+        {
+            Assert.Equal(true, (Convert.ToInt32(number) > 0));
+        }
 }
