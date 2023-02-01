cea2ac3f774c4dd7e080be772b63deb74dcbd4cf
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-02-01 13:32:16 +0100

    Update ClassData.cs

--- TestProject1/ClassData.cs
+++ TestProject1/ClassData.cs
@@ -8,34 +8,34 @@ using Xunit;
 
 namespace TestProject1
 {
-    
-public class EmployeeTestData:IEnumerable<object[]>
-    {
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            yield return new object[] {5, 1, 3, 9000};
-            yield return new object[] {6, 1, 3, 9000};
-
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-public class ParameterizedTests
-    
+public class TestDataGenerator : IEnumerable<object[]>
 {
-        public bool IsOddNumber(int number)
+    private readonly List<object[]> _data = new List<object[]>
+    {
+       List.Add(5);
+       List.Add(6);
+    };
+
+    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class ParameterizedTests
+{
+    public bool IsOddNumber(int number)
     {
         return number % 2 != 0;
     }
-    
-[Theory]
-    [ClassData(typeof(EmployeeTestData))]
-    public void Employee_GetFullName_Throw_ArgumentNullException_When_FirstName_Is(int a, int b, int c, int d)
-        {
+
+    [Theory]
+    [ClassData(typeof(TestDataGenerator))]
+    public void AllNumbers_AreOdd_WithClassData(int a)
+    {
         Assert.True(IsOddNumber(a));
-        }
+        Assert.True(IsOddNumber(b));
+        Assert.True(IsOddNumber(c));
+        Assert.True(IsOddNumber(d));
+    }
 }
-    
 }
