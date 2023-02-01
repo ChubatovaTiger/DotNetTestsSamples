5cc9da77c76390d76cbd0309af6f082f09dd9a38
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-02-01 13:39:51 +0100

    Update ClassData.cs

--- TestProject1/ClassData.cs
+++ TestProject1/ClassData.cs
@@ -8,6 +8,13 @@ using Xunit;
 
 namespace TestProject1
 {
+        
+            public class TestData {
+      int Base {get;set;}
+      int Exponent {get;set;}
+      int ExpectedResult {get;set;}
+    }
+        
 public List<TestData> PowerOfTestData() {
         yield return new TestData { Base = 0, Exponent = 0, TestData = 0 };
         yield return new TestData { Base = 0, Exponent = 1, TestData = 0 };
@@ -15,6 +22,18 @@ public List<TestData> PowerOfTestData() {
         yield return new TestData { Base = 2, Exponent = 1, TestData = 2 };
         yield return new TestData { Base = 5, Exponent = 2, TestData = 25 };
     }
+        
+        class PowerOfTestDataClass : TheoryData<int, int, int>
+{
+    public PowerOfTestDataClass()
+    {
+       Add(0, 0, 0);
+       Add(0, 1, 0);
+       Add(2, 0, 1);
+       Add(2, 1, 2);
+       Add(5, 2, 25);
+    }
+}
 
 public class ParameterizedTests
 {
@@ -24,7 +43,7 @@ public class ParameterizedTests
     }
 
     [Theory]
-    [ClassData(typeof(PowerOfTestData))]
+    [ClassData(typeof(PowerOfTestDataClass))]
     public void AllNumbers_AreOdd_WithClassData(int a, int b, int c)
     {
         Assert.True(IsOddNumber(a));
