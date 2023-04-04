a1c95f2283a8352cb56c4b5ed3a9516ae6f311ed
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-04 19:24:09 +0200

    Update NUnitTest2.cs

--- TestProject2/NUnitTest2.cs
+++ TestProject2/NUnitTest2.cs
@@ -4,58 +4,10 @@ using System.Threading;
 
 namespace TestProject2
 {
-    
-[TestFixture(1, 2)]
-    public class UnitTest12(int a, int b)
+    [TestFixture(1, 2)]
+    public class NUnitTest2(int a, int b)
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
- //[Ignore("Ignore a test")]
-        [Test]
-        public void Test1NunitSimplever1()
-        {
-            Thread.Sleep(11);
-            Assert.Fail();
-        }
-        
-         //[Ignore("Ignore a test")]
-        [Test]
-         [Category("Short")]
-        [TestCase("chrome", "72.0", "Windows 10")]
-        [TestCase("internet explorer", "11.0", "Windows 10")]
-        public void Test2(string str1, string str2, string str3)
-        {
-            Thread.Sleep(10);
-            Assert.Pass();
-        }
-        
-
-         //[Ignore("Ignore a test")]
-        [Test, Sequential]
-         [Category("Long")]
-        public void NunitSequentialParametersTest(
-         [Values(1,2,3)] int x,
-        [Values("A","B")] string s)
-        {
-            Thread.Sleep(10);
-         Assert.Pass();
-        }
-
-     //[Ignore("Ignore a test")]    
-[Test]
-[Category("Long")]
-public void NUnitRangeTest(
-    [Values(1, 2, 3)] int x,
-    [Values(1, 2, 3)] int y)
-{
-    Thread.Sleep(10);
-    Assert.Pass();
-}
-     
-        
-        
-        
+        [TestCase(3, 4)]
+        public void MyTest(int c, d) {}
     }
 }
