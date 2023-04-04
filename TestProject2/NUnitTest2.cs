77d9a6a1999966457177dac37b7d09ef63f880b7
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-04 19:40:06 +0200

    Update NUnitTest2.cs

--- TestProject2/NUnitTest2.cs
+++ TestProject2/NUnitTest2.cs
@@ -5,8 +5,17 @@ using System.Threading;
 namespace TestProject2
 {
     [TestFixture(1, 2)]
-    public class NUnitTest2(int a, int b){
+         private readonly T1 _t1;
+    private readonly T2 _t2;
+    public class NUnitTest2(int a, int b)
+
+    {
+        _t1 = a;
+        _t2 = b;
+    }
+
+
         [TestCase(3, 4)]
         public void MyTest(int c, d) {}
-    }
+
 }
