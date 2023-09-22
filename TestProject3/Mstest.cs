2cd4a4c6c89f351658e365768ea8790c97da1409
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-09-22 12:57:02 +0200

    Update Mstest.cs

--- TestProject3/Mstest.cs
+++ TestProject3/Mstest.cs
@@ -12,7 +12,7 @@ namespace TestProject3
         
         [DataTestMethod]
         [DataRow( "Aa", "Bb")]
-        [DataRow( "Xx", "Yy")] baa
+        [DataRow( "Xx", "Yy")] 
         public void MSTestDataRowTestMethod2(string str1, string str2)
         {
         }
