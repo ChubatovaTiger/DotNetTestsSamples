d5c2f616aaf6c407dbcb4fbf4d800b8a7a9ec491
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2025-11-04 20:28:24 +0100

    Update NUnitTest.cs

--- TestProject2/NUnitTest.cs
+++ TestProject2/NUnitTest.cs
@@ -10,5 +10,10 @@ namespace TestProject2
         {
            Assert.That(2 + 2, Is.EqualTo(4));
         }
+        [Test, Retry(5)]
+public void TestThatSometimesFails()
+{
+    Assert.That(new Random().Next(0, 2) == 1);
+}
     }
 }
