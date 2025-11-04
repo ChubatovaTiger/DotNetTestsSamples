221ac024a72c013688de2f744f8639ef624eb2ac
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2025-11-04 21:01:24 +0100

    Update NUnitTest.cs

--- TestProject2/NUnitTest.cs
+++ TestProject2/NUnitTest.cs
@@ -8,12 +8,12 @@ namespace TestProject2
     {
         [Test]
         public void Addition_ShouldReturnCorrectSum()
-        {Thread.Sleep(TimeSpan.FromMinutes(1));
+        {
            Assert.That(2 + 2, Is.EqualTo(4));
         }
         [Test]
 public void TestThatSometimesFails()
-{
+{Thread.Sleep(TimeSpan.FromMinutes(1));
     Assert.That(new Random().Next(0, 2) == 1);
 }
     }
